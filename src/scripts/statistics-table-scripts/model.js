45db3db9c0a939a37c219cc330e99b5923d38166
Srr233 <Sr233@yandex.ru>
2020-12-15 21:12:49 +0300

    feat: add model

---
+++ src/scripts/statistics-table-scripts/model.js
@@ -0,0 +1,28 @@
+class Model {
+  constructor() {
+    this.links = {
+      summary: fetch('https://api.covid19api.com/summary')
+
+    };
+    this.currentCountry = null;
+  }
+
+  getAllData() {
+    return Promise.all(Object.values(this.links)).then(arr => arr.map(response => response.json()));
+  }
+
+  getCurrentCountry(countryName) {
+    this.currentCountry = countryName;
+    return fetch('https://api.covid19api.com/summary')
+      .then(r => r.json(), err => {
+        throw new Error('Country not found, check the name: ' + err);
+      })
+      .then(arr => {
+        const res = arr.Countries.find(item => item.Country === countryName);
+        if (res) return res;
+        throw new Error('Country not found!');
+      });
+  }
+}
+
+export default Model;
