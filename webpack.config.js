0ddb4d8ee847f9f9027ae7034f7c79996a1d965f
Dmitry Astapenko <dmitryastapenko1@gmail.com>
2020-12-16 15:32:23 +0300

    feat: add css file handling

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,14 @@ module.exports = {
         }
       },
       {
+        test: /\.(css)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }]
+      },
+      {
         test: /\.(scss)$/,
         use: [{
           loader: 'style-loader'
