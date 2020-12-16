c459660a1d4eecbbe75027ee7eeaaf27bb381925
Srr233 <Sr233@yandex.ru>
2020-12-16 14:43:11 +0300

    refactor: refactor code

--- src/scripts/statistics-table-scripts/controller.js
+++ src/scripts/statistics-table-scripts/controller.js
@@ -3,15 +3,14 @@ import Model from './model.js';
 class Controller {
   constructor() {
     this.model = new Model();
-    this.test = null;
   }
 
   setCountry(countryCode) {
     this.model.setCurrentCountry(countryCode);
   }
 
-  test() {
-    this.test = false;
+  setGlobal() {
+    this.model.setAllData();
   }
 }
 
--- src/scripts/statistics-table-scripts/model.js
+++ src/scripts/statistics-table-scripts/model.js
@@ -1,24 +1,22 @@
 import View from './view.js';
+import { forModel } from './services.js';
 
 class Model {
   constructor() {
     this.view = new View();
-    this.links = {
-      summary: fetch('https://api.covid19api.com/summary')
-
-    };
+    this.summary = fetch('https://api.covid19api.com/summary');
     this.currentCountry = {};
   }
 
   async setAllData() {
-    return Promise.all(Object.values(this.links)).then(arr => arr.map(response => response.json()));
+    const json = await forModel.getAllData(this.summary);
+    this.view.updateTable(json);
   }
 
   async setCurrentCountry(countryCode) {
     this.currentCountryCode = countryCode;
 
-    const response = await this.links.summary;
-    const json = await response.json();
+    const json = await forModel.getAllData(this.summary);
     const currentCountry = await new Promise((resolve, reject) => {
       const res = json.Countries.find(item => item.CountryCode === countryCode);
       if (res) {
