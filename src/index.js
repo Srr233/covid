d9bfc1233fc48da8421c03b351a6a890c7f6590b
taniatat <tatsiana.tatartsava@gmail.com>
2020-12-22 18:16:33 +1100

    feat: start integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import CovidDashboardApp from './scripts/covid-dashboard-app';
 import './styles/index.scss';
 import './styles/graph-component.scss';
+import './styles/cases-for-countries.scss';
 
 CovidDashboardApp.initialize();
